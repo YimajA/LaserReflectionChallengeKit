e15e3c96e26b133bb0fbb2f55c6cdc21ed7cd24a
letuyettam5 <60398019+letuyettam5@users.noreply.github.com>
2023-10-04 16:50:10 -0400

    implement game generation

--- functionality/gameGeneration.cpp
+++ functionality/gameGeneration.cpp
@@ -1,11 +1,33 @@
 #include "gameGeneration.h"
 #include<cstdlib>
 #include<iostream>
-
+GameGenerator::GameConfig initGame;
+// Assuming the initial position (0 degree) of the mirrors is the result
+// When game starts, some mirrors will randomly move to random angles
 GameGenerator::GameConfig GameGenerator::generateGame() {
     // Todo
     // Providing a seed value
 	srand((unsigned) time(NULL));
-    int random = rand() % 360;
+    int numMirrorsDisabled = rand() % 2;
+
+    GameConfig initial = new GameConfig(); 
+
+    //un-disable all mirrors
+    for (int i = 0; i < 6; i++) {
+        GameConfig::mirrors[i].disabled = false;
+    }
+
+    // randomly select no more than 2 mirrors
+    for (int i = 0; i < numMirrorsDisabled; i++) {
+        int mirrorSelect = rand() % 6;
+        GameConfig::mirrors[mirrorSelect].disabled = true;
+    }
+
+    for (int i = 0; i < 6; i++) {
+        if (GameConfig::mirrors[i].disabled == false) {
+            int random = rand() % 360;
+            GameConfig::mirrors[i].startingPosition = random;
+        }
+    }
     return {};
 }
