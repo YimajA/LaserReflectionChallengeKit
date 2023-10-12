45fdd57399571e656eeee521b82863dd1045a602
YimajA <141763242+YimajA@users.noreply.github.com>
2023-10-12 17:44:57 -0400

    Main Attempt to Test User Interface

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #undef __ARM_FP
 #include "mbed.h"
 
+
 // main() runs in its own thread in the OS
 int main()
 {
     
-
+    //initialize the hardware
 
 
     // End Setup
@@ -13,8 +14,16 @@ int main()
     while (true) {
         // Start Loop
         
-        
+        // Read user input and test the UserInterface functions
+        int selectedMirror = UserInterface::getMirrorSelected();
+        int mirrorRotation = UserInterface::getMirrorRotation();
+        bool buttonPushed = UserInterface::isButtonPushed();
 
+        // Print out the selected mirror and rotation for testing
+        printf("Selected Mirror: %d\n", selectedMirror);
+        printf("Mirror Rotation: %d degrees\n", mirrorRotation);
+        printf("Button Pushed: %s\n", buttonPushed ? "Yes" : "No");
+        
         // End Loop
     }
 }
