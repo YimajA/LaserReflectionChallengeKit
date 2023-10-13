45c701f6abcbc5f2bccf78c991adc1816582565c
YimajA <141763242+YimajA@users.noreply.github.com>
2023-10-13 13:02:45 -0400

    Update mirrorModule.cpp

--- functionality/mirrorModule.cpp
+++ functionality/mirrorModule.cpp
@@ -1,30 +1,34 @@
 #include "mirrorModule.h"
-//Yimaj Ahmed
 
 MirrorModule::MirrorModule(Servo* servo, AddressableLEDStrip* leds, int LEDIndex) {
-    // Todo
-
+    // Initialize attributes
+    this->servo = servo;
+    this->leds = leds;
+    this->LEDIndex = LEDIndex;
+    this->mode = Mode::DEFAULT; // Set the default mode when the mirror is created.
+    this->angle = 0; // Initialize angle to 0 degrees.
+    this->updateLED(); // Initialize LED to reflect the default mode.
 }
 
 void MirrorModule::setMode(Mode mode) {
     // Todo
+    this->mode = mode;
+    this->updateLED(); // Update the LED to reflect the new mode.
 }
 
 MirrorModule::Mode MirrorModule::getMode() {
     // Todo
-
-    return Mode::DEFAULT;
+    return this->mode;
 }
 
 void MirrorModule::setAngle(int degrees) {
-    
-    
-    
+    // Ensure that degrees stay within the 0-180 range
+    degrees = std::max(0, std::min(180, degrees));
+    this->angle = degrees;
 }
 
 // gets the angle in degrees
 int MirrorModule::getAngle() {
     // Todo
-
-    return 0;
+    return this->angle;
 }
